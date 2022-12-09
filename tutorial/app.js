f201fc91d6524b2ed2eb5b46a95a1619ab2d8e4b
Matias Bensan Araya <mbensan@gmail.com>
2022-12-09 03:59:35 -0300

    Carga dinamica de datos

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,15 +1,6 @@
 const nav = document.querySelector('#nav')
 const menu = document.querySelector('#menu')
 
-const login = () => {
-  
-  if (usernameInput.value === 'user' && passwordInput.value === 'pass') {
-    //ons.notification.alert('Correct!');
-    nav.resetToPage('pages/home.html')
-  } else {
-    ons.notification.alert('Wrong username/password combination');
-  }
-}
 
 function openMenu() {
   menu.open()
